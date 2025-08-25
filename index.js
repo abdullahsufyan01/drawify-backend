bd6d431bd5598c711d810af68fecfc0b3c99354d
abdullahsufyan01 <malickabee01@gmail.com>
2025-08-25 20:43:59 +0500

    added jazz cash and easypaisa end points

--- index.js
+++ index.js
@@ -13,5 +13,17 @@ connectToMongoDB();
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 
+// JazzCash callback
+app.post("/api/payment/jazzcash/callback", (req, res) => {
+  console.log("JazzCash Callback:", req.body);
+  res.status(200).send("Callback received");
+});
+
+// Easypaisa callback
+app.post("/api/payment/easypaisa/callback", (req, res) => {
+  console.log("Easypaisa Callback:", req.body);
+  res.status(200).send("Callback received");
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
