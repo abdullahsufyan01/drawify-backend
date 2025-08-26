0e3cc0cb5e7244d8fbb49a1997cc9fb161239e6c
abdullahsufyan01 <malickabee01@gmail.com>
2025-08-26 17:10:33 +0500

    index to server

--- server.js
+++ server.js
@@ -46,6 +46,5 @@ app.get('/', (req, res) => {
 });
 app.post('/test', (req, res) => res.json({ ok: true }));
 
-
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
